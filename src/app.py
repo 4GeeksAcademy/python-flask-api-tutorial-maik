882d0385a51fad76303eaf7d4ecc52ac60bc1e03
Maikel Guerra Pantoja <154547587+Maikgp@users.noreply.github.com>
2024-04-15 18:34:42 +0000

    listo

--- src/app.py
+++ src/app.py
@@ -1,25 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
+
+
+@app.route('/todos', methods=['GET'])
+def handle_todos():
+    my_json = jsonify(todos)
+    return my_json
+
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    print("Incoming request with the following body", request_body)
+    todos.append(request_body)
+    response_body = todos
+    return response_body
+
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.pop(position)
+    return jsonify(todos)
 
 todos = [
     { "label": "My first task", "done": False },
     { "label": "My second task", "done": False }
 ]
 
-@app.route('/todos', methods=['GET'])
-def hello_world():
 
 
 
 
-    # Puedes convertir esa variable en una cadena json de la siguiente manera
-    json_text = jsonify(todos)
-
-    # Y luego puedes devolverlo al front-end en el cuerpo de la respuesta de la siguiente manera
-    return json_text
-
-
-# Estas dos líneas siempre deben estar al final de tu archivo app.py
-
+# Estas dos líneas siempre seben estar al final de tu archivo app.py.
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
